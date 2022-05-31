8e91ebc9943cd36f0c1d4c20027974c0ed651517
asSqr <ryo.yossy@live.jp>
2022-05-31 19:35:17 +0900

    ABC253 E

--- ABC253 E.cpp
+++ ABC253 E.cpp
@@ -40,7 +40,91 @@ template<class T>
 std::ostream &operator<< ( std::ostream& out, const std::vector<T>& a )
 { std::cout << '['; rep( i, a.size() ){ std::cout << a[i]; if( i != a.size()-1 ) std::cout << ", "; } std::cout << ']'; return out; }
 
+const ll mod = 998244353;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+std::istream& operator>>(std::istream& is, mint& a) { return is >> a.x;}
+std::ostream& operator<<(std::ostream& os, const mint& a) { return os << a.x;}
+
+struct combination {
+  std::vector<mint> fact, ifact;
+  combination(int n):fact(n+1),ifact(n+1) {
+    assert(n < mod);
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
+  }
+  mint operator()(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n]*ifact[k]*ifact[n-k];
+  }
+};
+
+ll N, M, K;
+mint dp[1010][5010];
+
 int main() {
+    std::cin >> N >> M >> K;
+
+    dp[1][0] = 0;
+
+    repi( j, 1, M+1 )
+        dp[1][j] = j;
+
+    repi( i, 1, N ) {
+        repi( j, 1, M+1 ) {
+            if( !K ) {
+                dp[i+1][j] += dp[i][M];
+
+                continue;
+            }
+
+            if( j-K >= 1 ) {
+                ll lb = std::max(0ll, j-K);
+                dp[i+1][j] += dp[i][lb];
+            }
+
+            if( j+K <= M ) {
+                ll ub = std::min(j+K, M);
+                dp[i+1][j] += dp[i][M]-(ub ? dp[i][ub-1] : 0);
+            }
+        }
+
+        repi( j, 1, M )
+            dp[i+1][j+1] += dp[i+1][j];
+
+        dp[i+1][0] = 0;
+    }
+
+    std::cout << dp[N][M].x << endl;
 
     return 0;
 }
