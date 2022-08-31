3d1a702aa4f5e87b372c3cc94ac2f31cedd0973c
asSqr <ryo.yossy@live.jp>
2022-08-31 19:56:05 +0900

    ABC266 G 途中

---
+++ ABC266 G.cpp
@@ -0,0 +1,92 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cassert>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <vector>
+#include <atcoder/all>
+#define repi(i,a,b) for(ll i=(a);i<(b);++i)
+#define rep(i,a) repi(i,0,a)
+#define repdi(i,a,b) for(ll i=(a)-1;i>=(b);--i)
+#define repd(i,a) repdi(i,a,0)
+#define itr(it,a) for( auto it = (a).begin(); it != (a).end(); ++it )
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define endl '\n'
+#define debug(x) std::cerr << #x << " = " << (x) << endl;
+
+using ll = long long;
+using P = std::pair<ll, ll>;
+
+constexpr ll INF = 1ll<<60;
+constexpr ll mod = 998244353;
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+template<class S, class T>
+std::ostream& operator<< ( std::ostream& out, const std::pair<S,T>& a )
+{ std::cout << '(' << a.first << ", " << a.second << ')'; return out; }
+
+template<class T>
+std::ostream &operator<< ( std::ostream& out, const std::vector<T>& a )
+{ std::cout << '['; rep( i, a.size() ){ std::cout << a[i]; if( i != a.size()-1 ) std::cout << ", "; } std::cout << ']'; return out; }
+
+using mint = atcoder::modint998244353;
+
+struct combination {
+  std::vector<mint> fact, ifact;
+  combination(int n):fact(n+1),ifact(n+1) {
+    assert(n < mod);
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
+  }
+  mint operator()(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n]*ifact[k]*ifact[n-k];
+  }
+};
+
+ll R, G, B, K;
+combination comb(10000010);
+std::vector<mint> r, g;
+
+int main() {
+    std::cin >> R >> G >> B >> K;
+
+    mint ans = 0;
+
+    rep( i, R+1 ) {
+        ll rm = R-i;
+        
+        r.emplace_back(comb(rm-1, K-1) * comb.ifact[i]);
+    }
+
+    rep( i, G+1 ) {
+        ll gm = G-i;
+        
+        g.emplace_back(comb(gm-1, K-1) * comb.ifact[i]);
+    }
+
+    auto res = atcoder::convolution(r, g);
+
+    rep( i, res.size() ) {
+        ans += res[i] * comb(i+K+B, B) * ;
+    }
+
+    std::cout << ans.val() << endl;
+
+    return 0;
+}
